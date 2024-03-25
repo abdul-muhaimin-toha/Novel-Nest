4a5fa1e9f5be94f4bd1181f2710c4424f0a3b045
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-03-25 19:45:49 +0600

    initial project setup done

---
+++ README.md
@@ -0,0 +1,6 @@
+# PH-M8-ASSIGNMENT-8
+
+## Milestone: 8, Assignment: 8 (Novel Nest). Project based assignment for Progmming Hero course.
+
+[Live Website Link]()  
+[Project Repository Link]()
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello Novel Nest!</h1>
+      <h1 className="play-fair-display text-3xl font-bold underline">
+        Hello Novel Nest!
+      </h1>
+    </>
+  );
+}
+
+export default App;
